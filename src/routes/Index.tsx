ecb0419830b53541f738ccaf1602511a55242e50
Tamás Vince Kornél <me@vinn.fun>
2023-08-31 19:56:36 +0200

    added icons

--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -1,3 +1,5 @@
+import { RxGrid, RxListBullet } from 'react-icons/rx';
+
 function Index() {
   return (
     <div className="flex-col mx-auto max-w-[1440px] p-3">
@@ -5,8 +7,12 @@ function Index() {
         <div className="flex justify-between items-center mx-auto max-w-[800px]">
           <h1 className="font-bold text-4xl">Prémium hirdetések</h1>
           <div className="flex items-center space-x-1 bg-gray-800 p-1 rounded-lg">
-            <div className="w-10 h-10 bg-gray-700 flex rounded-l-md">Card</div>
-            <div className="w-10 h-10 bg-gray-800 flex rounded-r-md hover:bg-gray-600">list</div>
+            <div className="w-10 h-10 bg-gray-700 flex items-center justify-center rounded-l-md transition">
+              <RxListBullet size="24" />
+            </div>
+            <div className="w-10 h-10 bg-gray-800 flex items-center justify-center rounded-r-md hover:bg-gray-600 transition">
+              <RxGrid size="24" />
+            </div>
           </div>
         </div>
       </div>
